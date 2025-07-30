e68dfa78a13eeb194c2259f1df311180c4db2814
Ripple Domain <mfurkandemir554223@gmail.com>
2025-07-30 21:33:42 +0300

    Add project files.

---
+++ CPU-Optimization/src/main.cpp
@@ -0,0 +1,15 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+
+	return 0;
+}
